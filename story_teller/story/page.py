31605c14a672d8bf8bb7622c7176ffced37c1ecb
Sebastián Bórquez <sebastian.borquez.g@gmail.com>
2023-12-10 02:29:50 -0300

    Add page repository

--- story_teller/story/page.py
+++ story_teller/story/page.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from enum import StrEnum
-from typing import Optional
+from typing import Dict, Optional
 import uuid
 
 import pydantic
@@ -9,16 +9,21 @@ import pydantic
 class Image(pydantic.BaseModel):
     """Image for the page."""
 
+    # Local path to the image.
     path: Optional[str] = pydantic.Field(default=None)
+    # Remote url to the image.
     url: Optional[str] = pydantic.Field(default=None)
+    # Alt text for the image. Should be the same as in the description.
     description: Optional[str] = pydantic.Field(default=None)
 
     def load(self):
         """Load the image from the path."""
+        # TODO: Implement
         pass
 
     def save(self):
         """Save the image to the path."""
+        # TODO: Implement
         pass
 
 
@@ -27,10 +32,8 @@ class Description(pydantic.BaseModel):
 
     # The description of the currect state of the world.
     page: str
-
     # The description of the consequences of the action
     action: str
-
     # A visual description of the current state of the world.
     image: str
 
@@ -84,17 +87,74 @@ class PageType(StrEnum):
 
     # The page is the start of the story.
     START = "start"
+    # The page is a normal page.
+    ACTION = "action"
     # The page is the end of the story.
     END = "end"
-    # The page is a normal page.
-    NORMAL = "normal"
 
 
 class Page(pydantic.BaseModel):
     """A page in the story."""
     uuid: str = pydantic.Field(default_factory=lambda: uuid.uuid4().hex)
-    page_type: PageType = pydantic.Field(default=PageType.NORMAL)
+    page_type: PageType = pydantic.Field(default=PageType.ACTION)
     action: str = pydantic.Field(min_length=1, max_length=100)
     karma: KarmaPoints
     description: Optional[Description] = pydantic.Field(default=None)
     image: Optional[Image] = pydantic.Field(default=None)
+
+
+class PageRepository:
+    """A repository of pages."""
+    def __init__(self, pages: Dict[str, Page] = {}) -> None:
+        self.pages = pages
+
+    @classmethod
+    def from_dict(cls, pages_data: Dict[str, Page]) -> PageRepository:
+        """Create a page repository from a dictionary."""
+        pages = {}
+        for page_uuid, page_data in pages_data.items():
+            page_type = PageType(page_data["page_type"])
+            karma = KarmaPoints(**page_data["karma"])
+            description = Description(**page_data["description"])
+            image = Image(**page_data["image"]) if page_data["image"] else None
+            page = Page(
+                uuid=page_uuid,
+                page_type=page_type,
+                action=page_data["action"],
+                karma=karma,
+                description=description,
+                image=image,
+            )
+            pages[page_uuid] = page
+        return cls(pages=pages)
+
+    @classmethod
+    def to_dict(cls, repository: PageRepository) -> Dict[str, Page]:
+        """Create a dictionary from a page repository."""
+        pages = {}
+        for page_uuid, page in repository.pages.items():
+            pages[page_uuid] = {
+                "uuid": page.uuid,
+                "page_type": page.page_type.value,
+                "action": page.action,
+                "karma": page.karma.model_dump(),
+                "description": page.description.model_dump(),
+                "image": page.image.model_dump() if page.image else None,
+            }
+        return pages
+
+    def add_page(self, page: Page, safe: bool = True) -> None:
+        """Add a page to the repository."""
+        if safe and page.uuid in self.pages:
+            raise ValueError(f"Page with uuid {page.uuid} already exists.")
+        self.pages[page.uuid] = page
+
+    def get_page(self, uuid: str) -> Page:
+        """Get a page from the repository."""
+        return self.pages[uuid]
+
+    def remove_page(self, uuid: str) -> None:
+        """Remove a page from the repository."""
+        if uuid not in self.pages:
+            raise ValueError(f"Page with uuid {uuid} does not exist.")
+        del self.pages[uuid]
